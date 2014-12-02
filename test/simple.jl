fb8732fe203fef2f9cc2b781367c6c4be48901bf
Jack Lepird <jack.lepird@gmail.com>
2014-12-02 15:56:17 -0500

    Don't test dual formulation yet

--- test/simple.jl
+++ test/simple.jl
@@ -6,7 +6,7 @@ expr = Experiment()
 proc1 = TrainingProcess(rand(5,2), rand(5,1))
 push!(expr, proc1)
 
-proc2 = TrainingProcess(rand(5,2),rand(5,1),rls=Dual)
-push!(expr, proc2)
+# proc2 = TrainingProcess(rand(5,2),rand(5,1),rls=Dual)
+# push!(expr, proc2)
 
 res = process(expr)
