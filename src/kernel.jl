06adfe7a92878835965835b8ef43a23094ee8f59
zowen <zowen@mit.edu>
2014-12-06 17:41:24 -0500

    Defining getKernelSpace for Gaussian

--- src/kernel.jl
+++ src/kernel.jl
@@ -22,33 +22,27 @@ function buildKernel(train::Training{Gaussian,LOOCV,Dual},sigma)
 	return k
 end
 
-<<<<<<< HEAD
-function getKernelSpace(k,train::Training)
-	if k == Linear
-		return [()]
-	elseif k == Gaussian
-		kerneldistance = square_distance(train.X',train.X')
-		n = size(kerneldistance,1)
 
-		dists = sort(vec(tril(kerneldistance,-1)))[(n^2+n+2)/2:end]
-		sigmamin = dists[round(0.5 + length(dists) * 0.1)]
-		sigmamax = maximum(dists)
-
-		if sigmamin <= 0
-			sigmamin = eps();
-		end
-		if sigmamax <= 0
-			sigmamax = eps();
-		end
-
-		q = (sigmamax/sigmamin)^(1/(train.options.nSigma-1));
-    	return sigmamin*(q.^(train.options.nSigma-1:-1:0));
-	else
-		error("Unknown Kernel")
-	end
-end
-=======
 getKernelSpace{P<:Paramsel}(train::Training{Linear,P,Dual}) = [()]
-getKernelSpace{P<:Paramsel}(train::Training{Gaussian,P,Dual}) = error("Not yet implemented")
+
+function getKernelSpace{P<:Paramsel}(train::Training{Gaussian,P,Dual})
+	kerneldistance = square_distance(train.X',train.X')
+	n = size(kerneldistance,1)
+
+	dists = sort(vec(tril(kerneldistance,-1)))[(n^2+n+2)/2:end]
+	sigmamin = dists[round(0.5 + length(dists) * 0.1)]
+	sigmamax = maximum(dists)
+
+	if sigmamin <= 0
+		sigmamin = eps();
+	end
+	if sigmamax <= 0
+		sigmamax = eps();
+	end
+
+	q = (sigmamax/sigmamin)^(1/(train.options.nSigma-1));
+	return sigmamin*(q.^(train.options.nSigma-1:-1:0));
+end
+
 getKernelSpace(x) = error("Unknown kernel of type $(typeof(x))")
->>>>>>> 507cb556d4d0487002eb6b794b97987f2b2b2bb3
+
