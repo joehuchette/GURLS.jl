ad30f63a9c07f111b510bfce9829a5ef0ceb5546
zowen <zowen@mit.edu>
2014-12-07 18:42:42 -0500

    comments to testGaussian

--- test/testGaussian.jl
+++ test/testGaussian.jl
@@ -1,23 +1,13 @@
 # A bare bones test to quickly route out syntax errors
 using GURLS
 
+#Load the data from problem set 1 of the course
 xTrain = readcsv(Pkg.dir("GURLS") * "/data/xTrain.csv")
 yTrain = readcsv(Pkg.dir("GURLS") * "/data/yTrain.csv")
 xTest  = readcsv(Pkg.dir("GURLS") * "/data/xTest.csv")
 yTest  = readcsv(Pkg.dir("GURLS") * "/data/yTest.csv")
-# x = readcsv("x.csv")
-# y = readcsv("y.csv")
 
-# p = randperm(150)
-# x = x[p,:]
-# y = y[p]
-
-# xTrain = x[1:100,:]
-# yTrain = y[1:100]
-
-# xTest = x[101:150,:]
-# yTest = y[101:150]
-
+#Center data
 xMeans = mean(xTrain,1)
 for i in 1:size(xTrain,1)
 	xTrain[i,:] -= xMeans
@@ -26,18 +16,28 @@ for i in 1:size(xTest,1)
 	xTest[i,:] -= xMeans
 end
 
-# xTrain = xTrain[1:4,:]
-# yTrain = yTrain[1:4,:]
-# xTest = xTest[1:4,:]
-# yTest = yTest[1:4,:]
-
+#Specify a training task which gives all of the specification of the model.
+#Form:
+#Training(xTrainData,yTrainData,kernel = KernelType(), rls = AlgorithmType())
 dual = Training(xTrain, yTrain, kernel = Gaussian(), rls = Dual())
+
+#Specify a prediction task which takes a training tast and an independent variable data set.
+#Form:
+#Prediction(NameTrainingTask,xTestData)
 pred = Prediction(dual, xTest)
+
+#Specify a performance task which takes a prediction task, a dependent variable data set, and a
+#performance metric to be used in the comparison.
+#Form:
+#Prediction(NameTrainingTask,xTestData)
 perf = Performance(pred, yTest, MacroAvg())
 
+#Form experiment, a series of tasks to be performed.
 ex = Experiment(dual, pred, perf)
 
+#Processes all of the tasks defined in the given Experiment in order.
 res = process(ex)
+
 m = res[dual].model
 
 println(typeof(m))
