a339d9b2c8b89e01b89263231d7d3450913eaffe
Jack Lepird <jack.lepird@gmail.com>
2014-12-07 23:17:40 -0500

    Demos now use Resampling package to split data into train/test sets

--- demo/iris.jl
+++ demo/iris.jl
@@ -1,12 +1,16 @@
+Pkg.add("Resampling")
+using Resampling
 using RDatasets
 using GURLS
 
 # Load the iris dataset
 iris = dataset("datasets","iris")
 
+iris_train, iris_test = splitrandom(iris, 0.7)
+
 # Regress SepalWidth on PetalLength, PetalWidth, and the Interaction between the two
-m = train(SepalWidth ~ PetalLength * PetalWidth,data = iris, kernel = Linear(),
+m = train(SepalWidth ~ PetalLength * PetalWidth,data = iris_train, kernel = Linear(),
 													 		 validation = LOOCV(),
 													 	     method = Dual())
 
-res = predict(m,iris)
+res = predict(m,iris_test)
