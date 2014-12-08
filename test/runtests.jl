e1df0a6f17c20832077fbc4d720550bfadf835b4
Jack Lepird <jack.lepird@gmail.com>
2014-12-07 21:35:17 -0500

    fixed typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-include(Pkg.dir("GURLS") * "/demos/linearKernel.jl")
-include(Pkg.dir("GURLS") * "/demos/gaussian.jl")
+include(Pkg.dir("GURLS") * "/demo/linearKernel.jl")
+include(Pkg.dir("GURLS") * "/demo/gaussian.jl")
 include(Pkg.dir("GURLS") * "/test/simple_legacy.jl")
 include(Pkg.dir("GURLS") * "/demo/iris.jl")
