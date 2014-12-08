0bb636c5dcaa10993b09ac6b4f18d1d55c95a5f2
Joey Huchette <joehuchette@gmail.com>
2014-12-07 23:32:54 -0500

    remove random old file and update legacy test

--- test/simple_legacy.jl
+++ test/simple_legacy.jl
@@ -1,15 +1,15 @@
 # A bare bones test to quickly route out syntax errors
 using GURLS.Legacy
 
-xTrain = readcsv("../data/xTrain.csv")
-yTrain = readcsv("../data/yTrain.csv")
-xTest = readcsv("../data/xTest.csv")
-yTest = readcsv("../data/yTest.csv")
+xTrain = readcsv(Pkg.dir("GURLS") * "../data/xTrain.csv")
+yTrain = readcsv(Pkg.dir("GURLS") * "../data/yTrain.csv")
+xTest  = readcsv(Pkg.dir("GURLS") * "../data/xTest.csv")
+yTest  = readcsv(Pkg.dir("GURLS") * "../data/yTest.csv")
 
 xMeans = mean(xTrain,1)
 for i in 1:size(xTrain,1)
 	xTrain[i,:] -= xMeans
-	xTest[i,:] -= xMeans
+	xTest[i,:]  -= xMeans
 end
 
 opt = defopt("simple legacy example")
