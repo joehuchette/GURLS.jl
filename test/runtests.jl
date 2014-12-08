4ec87d787ff9ff7c9d4e95499b3abd78226bac36
Jack Lepird <jack.lepird@gmail.com>
2014-12-07 21:56:46 -0500

    Another fix to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using RDatasets # has to go here, as it needs to be included before GURLS.jl
 include(Pkg.dir("GURLS") * "/demo/linearKernel.jl")
 include(Pkg.dir("GURLS") * "/demo/gaussian.jl")
 include(Pkg.dir("GURLS") * "/test/simple_legacy.jl")
